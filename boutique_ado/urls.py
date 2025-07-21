13de76635a2a9324656986c136be56268300a425
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-07-21 10:12:30 +0100

    setup allauth

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')), #include imported above (not imported by default!) + SLASH IS NEEDED SO IT WORKS CORRECTLY IN THE URL
 ]
