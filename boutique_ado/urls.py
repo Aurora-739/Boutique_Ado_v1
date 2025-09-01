22b738deed24be0887d6f61b9aa1439845da7a4d
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-09-01 12:29:24 +0100

    completed home page header & css

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')), #include imported above (not imported by default!) + SLASH IS NEEDED SO IT WORKS CORRECTLY IN THE URL
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
